d98f7ff2e8098402f891c65d436cf2b21f29ab86
carlo <the7thcapo@gmail.com>
2015-12-26 23:11:00 +1100

    Shifty Computers - System Calculator

---
+++ Shifty Computers System Calculator/Form1.Designer.vb
@@ -0,0 +1,330 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.gbCPU = New System.Windows.Forms.GroupBox()
+        Me.chkbxCPU1 = New System.Windows.Forms.CheckBox()
+        Me.chkbxCPU3 = New System.Windows.Forms.CheckBox()
+        Me.chkbxCPU2 = New System.Windows.Forms.CheckBox()
+        Me.gbRAM = New System.Windows.Forms.GroupBox()
+        Me.chkbxRAM3 = New System.Windows.Forms.CheckBox()
+        Me.chkbxRAM2 = New System.Windows.Forms.CheckBox()
+        Me.chkbxRAM1 = New System.Windows.Forms.CheckBox()
+        Me.gbHDD = New System.Windows.Forms.GroupBox()
+        Me.chkbxHDD3 = New System.Windows.Forms.CheckBox()
+        Me.chkbxHDD2 = New System.Windows.Forms.CheckBox()
+        Me.chkbxHDD1 = New System.Windows.Forms.CheckBox()
+        Me.gbScreen = New System.Windows.Forms.GroupBox()
+        Me.chkbxSCREEN3 = New System.Windows.Forms.CheckBox()
+        Me.chkbxSCREEN2 = New System.Windows.Forms.CheckBox()
+        Me.chkbxSCREEN1 = New System.Windows.Forms.CheckBox()
+        Me.lstbxOrders = New System.Windows.Forms.ListBox()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblAmount = New System.Windows.Forms.Label()
+        Me.btnOrder = New System.Windows.Forms.Button()
+        Me.lblExpect = New System.Windows.Forms.Label()
+        Me.lblExpectPrice = New System.Windows.Forms.Label()
+        Me.gbCPU.SuspendLayout()
+        Me.gbRAM.SuspendLayout()
+        Me.gbHDD.SuspendLayout()
+        Me.gbScreen.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'gbCPU
+        '
+        Me.gbCPU.Controls.Add(Me.chkbxCPU1)
+        Me.gbCPU.Controls.Add(Me.chkbxCPU3)
+        Me.gbCPU.Controls.Add(Me.chkbxCPU2)
+        Me.gbCPU.Location = New System.Drawing.Point(44, 67)
+        Me.gbCPU.Name = "gbCPU"
+        Me.gbCPU.Size = New System.Drawing.Size(141, 100)
+        Me.gbCPU.TabIndex = 0
+        Me.gbCPU.TabStop = False
+        Me.gbCPU.Text = "CPU"
+        '
+        'chkbxCPU1
+        '
+        Me.chkbxCPU1.AutoSize = True
+        Me.chkbxCPU1.Location = New System.Drawing.Point(6, 24)
+        Me.chkbxCPU1.Name = "chkbxCPU1"
+        Me.chkbxCPU1.Size = New System.Drawing.Size(81, 17)
+        Me.chkbxCPU1.TabIndex = 9
+        Me.chkbxCPU1.Text = "Intel G1840"
+        Me.chkbxCPU1.UseVisualStyleBackColor = True
+        '
+        'chkbxCPU3
+        '
+        Me.chkbxCPU3.AutoSize = True
+        Me.chkbxCPU3.Location = New System.Drawing.Point(6, 70)
+        Me.chkbxCPU3.Name = "chkbxCPU3"
+        Me.chkbxCPU3.Size = New System.Drawing.Size(130, 17)
+        Me.chkbxCPU3.TabIndex = 8
+        Me.chkbxCPU3.Text = "Intel Xeon E3-1231V3"
+        Me.chkbxCPU3.UseVisualStyleBackColor = True
+        '
+        'chkbxCPU2
+        '
+        Me.chkbxCPU2.AutoSize = True
+        Me.chkbxCPU2.Location = New System.Drawing.Point(6, 47)
+        Me.chkbxCPU2.Name = "chkbxCPU2"
+        Me.chkbxCPU2.Size = New System.Drawing.Size(87, 17)
+        Me.chkbxCPU2.TabIndex = 7
+        Me.chkbxCPU2.Text = "Intel i5-4460 "
+        Me.chkbxCPU2.UseVisualStyleBackColor = True
+        '
+        'gbRAM
+        '
+        Me.gbRAM.Controls.Add(Me.chkbxRAM3)
+        Me.gbRAM.Controls.Add(Me.chkbxRAM2)
+        Me.gbRAM.Controls.Add(Me.chkbxRAM1)
+        Me.gbRAM.Location = New System.Drawing.Point(260, 67)
+        Me.gbRAM.Name = "gbRAM"
+        Me.gbRAM.Size = New System.Drawing.Size(246, 100)
+        Me.gbRAM.TabIndex = 4
+        Me.gbRAM.TabStop = False
+        Me.gbRAM.Text = "RAM"
+        '
+        'chkbxRAM3
+        '
+        Me.chkbxRAM3.AutoSize = True
+        Me.chkbxRAM3.Location = New System.Drawing.Point(12, 70)
+        Me.chkbxRAM3.Name = "chkbxRAM3"
+        Me.chkbxRAM3.Size = New System.Drawing.Size(234, 17)
+        Me.chkbxRAM3.TabIndex = 12
+        Me.chkbxRAM3.Text = "16G Kit 1600 (8Gx2) Corsair Vengeance-LP "
+        Me.chkbxRAM3.UseVisualStyleBackColor = True
+        '
+        'chkbxRAM2
+        '
+        Me.chkbxRAM2.AutoSize = True
+        Me.chkbxRAM2.Location = New System.Drawing.Point(12, 47)
+        Me.chkbxRAM2.Name = "chkbxRAM2"
+        Me.chkbxRAM2.Size = New System.Drawing.Size(166, 17)
+        Me.chkbxRAM2.TabIndex = 11
+        Me.chkbxRAM2.Text = "8G Single 1866 Kingston-Fury"
+        Me.chkbxRAM2.UseVisualStyleBackColor = True
+        '
+        'chkbxRAM1
+        '
+        Me.chkbxRAM1.AutoSize = True
+        Me.chkbxRAM1.Location = New System.Drawing.Point(12, 24)
+        Me.chkbxRAM1.Name = "chkbxRAM1"
+        Me.chkbxRAM1.Size = New System.Drawing.Size(142, 17)
+        Me.chkbxRAM1.TabIndex = 10
+        Me.chkbxRAM1.Text = "4G Single 1333 Patriot-S"
+        Me.chkbxRAM1.UseVisualStyleBackColor = True
+        '
+        'gbHDD
+        '
+        Me.gbHDD.Controls.Add(Me.chkbxHDD3)
+        Me.gbHDD.Controls.Add(Me.chkbxHDD2)
+        Me.gbHDD.Controls.Add(Me.chkbxHDD1)
+        Me.gbHDD.Location = New System.Drawing.Point(44, 190)
+        Me.gbHDD.Name = "gbHDD"
+        Me.gbHDD.Size = New System.Drawing.Size(168, 100)
+        Me.gbHDD.TabIndex = 5
+        Me.gbHDD.TabStop = False
+        Me.gbHDD.Text = "HARD DRIVES"
+        '
+        'chkbxHDD3
+        '
+        Me.chkbxHDD3.AutoSize = True
+        Me.chkbxHDD3.Location = New System.Drawing.Point(6, 73)
+        Me.chkbxHDD3.Name = "chkbxHDD3"
+        Me.chkbxHDD3.Size = New System.Drawing.Size(147, 17)
+        Me.chkbxHDD3.TabIndex = 12
+        Me.chkbxHDD3.Text = "WD Red Pro SATA3 3TB"
+        Me.chkbxHDD3.UseVisualStyleBackColor = True
+        '
+        'chkbxHDD2
+        '
+        Me.chkbxHDD2.AutoSize = True
+        Me.chkbxHDD2.Location = New System.Drawing.Point(6, 50)
+        Me.chkbxHDD2.Name = "chkbxHDD2"
+        Me.chkbxHDD2.Size = New System.Drawing.Size(137, 17)
+        Me.chkbxHDD2.TabIndex = 11
+        Me.chkbxHDD2.Text = "Seagate 3.5"" NAS 2TB"
+        Me.chkbxHDD2.UseVisualStyleBackColor = True
+        '
+        'chkbxHDD1
+        '
+        Me.chkbxHDD1.AutoSize = True
+        Me.chkbxHDD1.Location = New System.Drawing.Point(6, 27)
+        Me.chkbxHDD1.Name = "chkbxHDD1"
+        Me.chkbxHDD1.Size = New System.Drawing.Size(145, 17)
+        Me.chkbxHDD1.TabIndex = 10
+        Me.chkbxHDD1.Text = "Seagate 3.5"" SSHD 1TB"
+        Me.chkbxHDD1.UseVisualStyleBackColor = True
+        '
+        'gbScreen
+        '
+        Me.gbScreen.Controls.Add(Me.chkbxSCREEN3)
+        Me.gbScreen.Controls.Add(Me.chkbxSCREEN2)
+        Me.gbScreen.Controls.Add(Me.chkbxSCREEN1)
+        Me.gbScreen.Location = New System.Drawing.Point(260, 190)
+        Me.gbScreen.Name = "gbScreen"
+        Me.gbScreen.Size = New System.Drawing.Size(246, 100)
+        Me.gbScreen.TabIndex = 6
+        Me.gbScreen.TabStop = False
+        Me.gbScreen.Text = "SCREENS"
+        '
+        'chkbxSCREEN3
+        '
+        Me.chkbxSCREEN3.AutoSize = True
+        Me.chkbxSCREEN3.Location = New System.Drawing.Point(6, 73)
+        Me.chkbxSCREEN3.Name = "chkbxSCREEN3"
+        Me.chkbxSCREEN3.Size = New System.Drawing.Size(238, 17)
+        Me.chkbxSCREEN3.TabIndex = 15
+        Me.chkbxSCREEN3.Text = "27"" 5ms 4k-UHD Asus PB279Q(3840x2160) "
+        Me.chkbxSCREEN3.UseVisualStyleBackColor = True
+        '
+        'chkbxSCREEN2
+        '
+        Me.chkbxSCREEN2.AutoSize = True
+        Me.chkbxSCREEN2.Location = New System.Drawing.Point(6, 50)
+        Me.chkbxSCREEN2.Name = "chkbxSCREEN2"
+        Me.chkbxSCREEN2.Size = New System.Drawing.Size(154, 17)
+        Me.chkbxSCREEN2.TabIndex = 14
+        Me.chkbxSCREEN2.Text = "27"" 4ms Benq GW2760HS"
+        Me.chkbxSCREEN2.UseVisualStyleBackColor = True
+        '
+        'chkbxSCREEN1
+        '
+        Me.chkbxSCREEN1.AutoSize = True
+        Me.chkbxSCREEN1.Location = New System.Drawing.Point(6, 27)
+        Me.chkbxSCREEN1.Name = "chkbxSCREEN1"
+        Me.chkbxSCREEN1.Size = New System.Drawing.Size(136, 17)
+        Me.chkbxSCREEN1.TabIndex = 13
+        Me.chkbxSCREEN1.Text = "24"" 2ms BenQ GL2460"
+        Me.chkbxSCREEN1.UseVisualStyleBackColor = True
+        '
+        'lstbxOrders
+        '
+        Me.lstbxOrders.FormattingEnabled = True
+        Me.lstbxOrders.Location = New System.Drawing.Point(535, 67)
+        Me.lstbxOrders.Name = "lstbxOrders"
+        Me.lstbxOrders.Size = New System.Drawing.Size(268, 225)
+        Me.lstbxOrders.TabIndex = 7
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(530, 332)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(122, 25)
+        Me.lblTotal.TabIndex = 8
+        Me.lblTotal.Text = "Total:  AUD"
+        '
+        'lblAmount
+        '
+        Me.lblAmount.AutoSize = True
+        Me.lblAmount.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAmount.Location = New System.Drawing.Point(738, 332)
+        Me.lblAmount.Name = "lblAmount"
+        Me.lblAmount.Size = New System.Drawing.Size(24, 25)
+        Me.lblAmount.TabIndex = 9
+        Me.lblAmount.Text = "0"
+        '
+        'btnOrder
+        '
+        Me.btnOrder.Location = New System.Drawing.Point(535, 371)
+        Me.btnOrder.Name = "btnOrder"
+        Me.btnOrder.Size = New System.Drawing.Size(75, 23)
+        Me.btnOrder.TabIndex = 10
+        Me.btnOrder.Text = "ORDER!"
+        Me.btnOrder.UseVisualStyleBackColor = True
+        '
+        'lblExpect
+        '
+        Me.lblExpect.AutoSize = True
+        Me.lblExpect.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblExpect.Location = New System.Drawing.Point(530, 295)
+        Me.lblExpect.Name = "lblExpect"
+        Me.lblExpect.Size = New System.Drawing.Size(156, 25)
+        Me.lblExpect.TabIndex = 11
+        Me.lblExpect.Text = "expected: AUD"
+        '
+        'lblExpectPrice
+        '
+        Me.lblExpectPrice.AutoSize = True
+        Me.lblExpectPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblExpectPrice.Location = New System.Drawing.Point(738, 295)
+        Me.lblExpectPrice.Name = "lblExpectPrice"
+        Me.lblExpectPrice.Size = New System.Drawing.Size(24, 25)
+        Me.lblExpectPrice.TabIndex = 12
+        Me.lblExpectPrice.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(831, 406)
+        Me.Controls.Add(Me.lblExpectPrice)
+        Me.Controls.Add(Me.lblExpect)
+        Me.Controls.Add(Me.btnOrder)
+        Me.Controls.Add(Me.lblAmount)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lstbxOrders)
+        Me.Controls.Add(Me.gbScreen)
+        Me.Controls.Add(Me.gbHDD)
+        Me.Controls.Add(Me.gbRAM)
+        Me.Controls.Add(Me.gbCPU)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.gbCPU.ResumeLayout(False)
+        Me.gbCPU.PerformLayout()
+        Me.gbRAM.ResumeLayout(False)
+        Me.gbRAM.PerformLayout()
+        Me.gbHDD.ResumeLayout(False)
+        Me.gbHDD.PerformLayout()
+        Me.gbScreen.ResumeLayout(False)
+        Me.gbScreen.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents gbCPU As System.Windows.Forms.GroupBox
+    Friend WithEvents chkbxCPU1 As System.Windows.Forms.CheckBox
+    Friend WithEvents chkbxCPU3 As System.Windows.Forms.CheckBox
+    Friend WithEvents chkbxCPU2 As System.Windows.Forms.CheckBox
+    Friend WithEvents gbRAM As System.Windows.Forms.GroupBox
+    Friend WithEvents chkbxRAM3 As System.Windows.Forms.CheckBox
+    Friend WithEvents chkbxRAM2 As System.Windows.Forms.CheckBox
+    Friend WithEvents chkbxRAM1 As System.Windows.Forms.CheckBox
+    Friend WithEvents gbHDD As System.Windows.Forms.GroupBox
+    Friend WithEvents chkbxHDD3 As System.Windows.Forms.CheckBox
+    Friend WithEvents chkbxHDD2 As System.Windows.Forms.CheckBox
+    Friend WithEvents chkbxHDD1 As System.Windows.Forms.CheckBox
+    Friend WithEvents gbScreen As System.Windows.Forms.GroupBox
+    Friend WithEvents chkbxSCREEN3 As System.Windows.Forms.CheckBox
+    Friend WithEvents chkbxSCREEN2 As System.Windows.Forms.CheckBox
+    Friend WithEvents chkbxSCREEN1 As System.Windows.Forms.CheckBox
+    Friend WithEvents lstbxOrders As System.Windows.Forms.ListBox
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents lblAmount As System.Windows.Forms.Label
+    Friend WithEvents btnOrder As System.Windows.Forms.Button
+    Friend WithEvents lblExpect As System.Windows.Forms.Label
+    Friend WithEvents lblExpectPrice As System.Windows.Forms.Label
+
+End Class
---
+++ Shifty Computers System Calculator/Form1.vb
@@ -0,0 +1,178 @@
+﻿Public Class Form1
+
+    Dim value1 As Decimal
+    Dim value2 As Decimal
+    Dim value3 As Decimal
+    Dim value4 As Decimal
+    Dim value5 As Decimal
+    Dim value6 As Decimal
+    Dim value7 As Decimal
+    Dim value8 As Decimal
+    Dim value9 As Decimal
+    Dim value10 As Decimal
+    Dim value11 As Decimal
+    Dim value12 As Decimal
+    Dim totalAmount As Decimal
+    Dim expectedAmount As Decimal
+
+
+    Private Sub chkbxCPU1_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxCPU1.CheckedChanged
+        If chkbxCPU1.Checked = True Then
+            lstbxOrders.Items.Add("Intel G1840          55")
+            value1 = 55
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxCPU1.Checked = False
+            lstbxOrders.Items.Remove("Intel G1840          55")
+            value1 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub chkbxCPU2_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxCPU2.CheckedChanged
+        If chkbxCPU2.Checked = True Then
+            lstbxOrders.Items.Add("Intel i5-4460            249")
+            value2 = 249
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxCPU2.Checked = False
+            lstbxOrders.Items.Remove("Intel i5-4460            249")
+            value2 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub chkbxCPU3_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxCPU3.CheckedChanged
+        If chkbxCPU3.Checked = True Then
+            lstbxOrders.Items.Add("Intel Xeon E3-1231V3         349")
+            value3 = 349
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxCPU3.Checked = False
+            lstbxOrders.Items.Remove("Intel Xeon E3-1231V3         349")
+            value3 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub chkbxRAM1_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxRAM1.CheckedChanged
+        If chkbxRAM1.Checked = True Then
+            lstbxOrders.Items.Add("4G Single 1333 Patriot-S         49 ")
+            value4 = 49
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxRAM1.Checked = False
+            lstbxOrders.Items.Remove("4G Single 1333 Patriot-S         49 ")
+            value4 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub chkbxRAM2_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxRAM2.CheckedChanged
+        If chkbxRAM2.Checked = True Then
+            lstbxOrders.Items.Add("8G Single 1866 Kingston-Fury         92 ")
+            value5 = 92
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxRAM2.Checked = False
+            lstbxOrders.Items.Remove("8G Single 1866 Kingston-Fury         92 ")
+            value5 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub chkbxRAM3_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxRAM3.CheckedChanged
+        If chkbxRAM3.Checked = True Then
+            lstbxOrders.Items.Add("16G Kit 1600 (8Gx2) Corsair Vengeance-LP         196 ")
+            value6 = 196
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxRAM3.Checked = False
+            lstbxOrders.Items.Remove("16G Kit 1600 (8Gx2) Corsair Vengeance-LP         196 ")
+            value6 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub chkbxHDD1_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxHDD1.CheckedChanged
+        If chkbxHDD1.Checked = True Then
+            lstbxOrders.Items.Add("Seagate 3.5in SSHD 1TB           99 ")
+            value7 = 99
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxHDD1.Checked = False
+            lstbxOrders.Items.Remove("Seagate 3.5in SSHD 1TB           99 ")
+            value7 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub chkbxHDD2_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxHDD2.CheckedChanged
+        If chkbxHDD2.Checked = True Then
+            lstbxOrders.Items.Add("Seagate 3.5in NAS 2TB            129 ")
+            value8 = 129
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxHDD2.Checked = False
+            lstbxOrders.Items.Remove("Seagate 3.5in NAS 2TB            129 ")
+            value8 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub chkbxHDD3_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxHDD3.CheckedChanged
+        If chkbxHDD3.Checked = True Then
+            lstbxOrders.Items.Add("WD Red Pro SATA3 3TB         234 ")
+            value9 = 234
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxHDD3.Checked = False
+            lstbxOrders.Items.Remove("WD Red Pro SATA3 3TB         234 ")
+            value9 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub chkbxSCREEN1_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxSCREEN1.CheckedChanged
+        If chkbxSCREEN1.Checked = True Then
+            lstbxOrders.Items.Add("24in 2ms BenQ GL2460         179 ")
+            value10 = 179
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxSCREEN1.Checked = False
+            lstbxOrders.Items.Remove("24in 2ms BenQ GL2460         179 ")
+            value10 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub chkbxSCREEN2_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxSCREEN2.CheckedChanged
+        If chkbxSCREEN2.Checked = True Then
+            lstbxOrders.Items.Add("27in 4ms Benq GW2760HS               329 ")
+            value11 = 329
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxSCREEN2.Checked = False
+            lstbxOrders.Items.Remove("27in 4ms Benq GW2760HS               329 ")
+            value11 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub chkbxSCREEN3_CheckedChanged(sender As Object, e As EventArgs) Handles chkbxSCREEN3.CheckedChanged
+        If chkbxSCREEN3.Checked = True Then
+            lstbxOrders.Items.Add("27in 5ms 4k-UHD Asus PB279Q(3840x2160)           999 ")
+            value12 = 999
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        Else
+            chkbxSCREEN3.Checked = False
+            lstbxOrders.Items.Remove("27in 5ms 4k-UHD Asus PB279Q(3840x2160)           999 ")
+            value12 = 0
+            lblExpectPrice.Text = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 + value11 + value12 & " "
+        End If
+    End Sub
+
+    Private Sub btnOrder_Click(sender As Object, e As EventArgs) Handles btnOrder.Click
+        lblAmount.Text = lblExpectPrice.Text
+    End Sub
+End Class
